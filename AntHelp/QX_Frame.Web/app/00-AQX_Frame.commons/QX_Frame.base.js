74b1fd84564d70a7b9453d5c0e43eca549c48101
qixiao <wd8622088@foxmail.com>
2016-11-26 09:35:10 +0800

    Update Frame - qixiao

--- AntHelp/QX_Frame.Web/app/00-AQX_Frame.commons/QX_Frame.base.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-class appBase {
-}
-appBase.AppName = "Ant Help"; //the app name
-appBase.DomainApi = "http://localhost:3999/"; //the web api Domain
-exports.appBase = appBase;
-//# sourceMappingURL=QX_Frame.base.js.map

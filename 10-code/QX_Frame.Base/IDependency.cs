dea206e89a5ad0ad598a8feae816e4b148626da9
qixiao <wd8622088@foxmail.com>
2016-12-28 00:22:14 +0800

    qxframe update2 delete dependency dll

---
+++ 10-code/QX_Frame.Base/IDependency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QX_Frame.Base
+{
+    /*
+        author:qixiao
+        time:2016-12-28 00:12:52
+    */
+
+    /// <summary>
+    /// dependency injection interface the implement class auto injection into the Ico vessel
+    /// </summary>
+    public interface IDependency
+    {
+    }
+}

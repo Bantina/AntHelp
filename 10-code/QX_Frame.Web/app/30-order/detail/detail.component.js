98c2a0430370cdc3a7878a4418f81b6439cea63d
tiantianbai <tiantianbai@sohu-inc.com>
2017-04-17 11:21:28 +0800

    publish ts

--- 10-code/QX_Frame.Web/app/30-order/detail/detail.component.js
+++ 10-code/QX_Frame.Web/app/30-order/detail/detail.component.js
@@ -7,9 +7,90 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
+const appBase_1 = require("../../00-AQX_Frame.commons/appBase");
+const appService_1 = require("../../00-AQX_Frame.services/appService");
 let OrderDetailComponent = class OrderDetailComponent {
+    constructor() {
+        this.order = {
+            orderUid: "",
+            publisherUid: "",
+            publishTime: "",
+            orderDescription: "",
+            orderCategoryId: "10",
+            receiverUid: "",
+            receiveTime: "",
+            orderStatusId: "",
+            orderValue: "0",
+            allowVoucher: "",
+            voucherMax: "",
+            evaluateUid: "",
+            address: "",
+            phone: "",
+            imageUrls: ""
+        };
+        this.orderPlus = {
+            publisherInfo: {},
+            receiverInfo: {},
+            orderCategory: {},
+            orderStatus: {},
+            orderEvaluate: {}
+        };
+    }
+    //立即抢单
+    getOrder() {
+    }
     ////the final execute ...
     ngOnInit() {
+        //var defaults = {
+        //    thumbSize: 20,
+        //    slideSpeed: 1500,
+        //    auto: true,
+        //    loop: true
+        //};
+        //$('.orderDetail_slider').tilesSlider($.extend({}, defaults, { x: 20, y: 1, effect: 'updown', cssSpeed: 500, backReverse: true }));
+        var self = this;
+        var orderUid = appService_1.appService.GetQueryString("orderUid");
+        $.ajax({
+            url: appBase_1.appBase.DomainApi + "api/Order/" + orderUid,
+            type: "get",
+            dataType: "json",
+            contentType: "application/json; charset=UTF-8",
+            data: {
+                //"id": orderUid,
+                "appKey": appService_1.appService.getCookie("appKey"),
+                "token": appService_1.appService.getCookie("token")
+            },
+            success(data) {
+                if (data.isSuccess) {
+                    self.order.publisherUid = data.data.publisherUid;
+                    self.order.orderUid = data.data.orderUid;
+                    self.order.publishTime = data.data.publishTime;
+                    self.order.orderDescription = data.data.orderDescription;
+                    self.order.orderCategoryId = data.data.orderCategoryId;
+                    self.order.receiverUid = data.data.receiverUid;
+                    self.order.receiveTime = data.data.receiveTime;
+                    self.order.orderStatusId = data.data.orderStatusId;
+                    self.order.orderValue = data.data.orderValue;
+                    self.order.allowVoucher = data.data.allowVoucher;
+                    self.order.voucherMax = data.data.voucherMax;
+                    self.order.evaluateUid = data.data.evaluateUid;
+                    self.order.address = data.data.address;
+                    self.order.phone = data.data.phone;
+                    self.order.imageUrls = data.data.imageUrls;
+                    self.orderPlus.publisherInfo = data.data.publisherInfo;
+                    self.orderPlus.receiverInfo = data.data.receiverInfo;
+                    self.orderPlus.orderCategory = data.data.orderCategory;
+                    self.orderPlus.orderStatus = data.data.orderStatus;
+                    self.orderPlus.orderEvaluate = data.data.orderEvaluate;
+                }
+                else {
+                    alert(data.msg);
+                }
+            },
+            error(data) {
+                alert("服务器错误！");
+            }
+        });
         var defaults = {
             thumbSize: 20,
             slideSpeed: 1500,
